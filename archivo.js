a8522ca31cda273c527978c5c4f37ed106d283d3
yung-woodz <dilan.toro40@gmail.com>
2024-04-04 22:32:30 -0300

    Primer programa el js

---
+++ archivo.js
@@ -0,0 +1,23 @@
+// let
+
+let nombre = "Dilan";
+console.log("Nombre: ", nombre);
+
+// const
+
+const edad = 21;
+console.log("Edad: ", edad);
+
+
+const frase = `${nombre}, que haces`;
+console.log(frase);
+
+
+let estudiantes = {
+    nombre: "Dilan",
+    edad: 21,
+    carrera: "IECI",
+    universidad: "Universidad del Biobio"
+}
+
+console.log(estudiantes.universidad);
